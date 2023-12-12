d46590bb94c964d72bd0c72c00348a5a2334c949
Nathaniel Dakin <nathanieldakin7@gmail.com>
2023-12-12 09:59:59 -0500

    Create rounded.java

---
+++ src/rounded.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+
+class RoundedPanel extends JPanel {
+    private int cornerRadius;
+
+    public RoundedPanel(int radius) {
+        super();
+        this.cornerRadius = radius;
+        setOpaque(false);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D graphics = (Graphics2D) g;
+        graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        graphics.setColor(getBackground());
+        graphics.fillRoundRect(0, 0, getWidth()-1, getHeight()-1, cornerRadius, cornerRadius);
+    }
+}
